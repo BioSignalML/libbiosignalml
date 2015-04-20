8f0aaeb37917977e0bb20078a5cab4143abc372a
David Brooks <dave@bcs.co.nz>
2015-04-20 21:16:37 +0200

    `model.h` is now the main header file.

--- src/model.h
+++ src/model.h
@@ -1,8 +1,13 @@
 #ifndef BSML_MODEL_H
 #define BSML_MODEL_H
 
-
+#include "ontology.h"
+#include "formats.h"
+#include "object.h"
+#include "timing.h"
+#include "signal.h"
+#include "event.h"
 #include "recording.h"
-
+#include "annotation.h"
 
 #endif
