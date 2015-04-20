a6fe6df34969ef86558b350c462c2a3bbce375d5
David Brooks <dave@bcs.co.nz>
2015-04-20 21:15:03 +0200

    Actually define include guard...

--- src/ontology.h
+++ src/ontology.h
@@ -7,6 +7,7 @@ Full documentation of the ontology is at http://www.biosignalml.org/ontologies/2
 **/
 
 #ifndef BSML_ONTOLOGY_H
+#define BSML_ONTOLOGY_H
 
 #include "typedobject/rdf.h"
 #include "typedobject/rdfdefs.h"
