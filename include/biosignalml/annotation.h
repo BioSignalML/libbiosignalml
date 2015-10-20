176ae14fc55485bbc56f4ba281a2cde932403431
David Brooks <dave@bcs.co.nz>
2015-10-20 14:44:24 +1300

    An annotation's subject (what it's `about`) can be any BioSignalML object.

--- include/biosignalml/annotation.h
+++ include/biosignalml/annotation.h
@@ -35,7 +35,7 @@ namespace bsml {
   {
     TYPED_OBJECT(Annotation, BSML::Annotation)
 
-    PROPERTY_URI(about, DCT::subject)
+    PROPERTY_REFERENCE(about, DCT::subject, Object)
     PROPERTY_NODE_SET(tags, BSML::tag)
     PROPERTY_OBJECT(annotation_time, BSML::time, TemporalEntity)
 
